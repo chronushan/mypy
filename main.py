fdaed6a886d7f97852594ad3fc6e56a684699f78
chronushan <48421873+chronushan@users.noreply.github.com>
2023-03-09 21:44:21 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = input.split(,)def twoSum():
+    input_string = input('nums =')
+    nums = input_string.split(',')
+    result = []
+
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+
+    target = int(input("target ="))
+
+    check = False
+    for i in range(1,len(nums)):
+        for j in range(i):
+            if (nums[i] + nums[j]) == target:
+                check = True
+                result.extend([i,j])
+                break
+        if check:
+            break
+
+    print(result)
